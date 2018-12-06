b4e6195e3433158cb5e56223a93fabbd46472dae
tyleung <tyleung@ualberta.ca>
2018-12-06 15:20:54 +0800

    Add currency formatter

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,75 +1,72 @@
-import { Countries, countries_EU } from "./countries";
-import * as Datamap from "datamaps";
-import { getTournamentAggregate, getTournamentByAggregateTime } from "./api";
+import { Countries, countries_EU } from './countries';
+import * as Datamap from 'datamaps';
+import { formatter } from './utils';
+import { getTournamentAggregate, getTournamentByAggregateTime } from './api';
 
 //basic map config with custom fills, mercator projection
 var countries_EU_shortcode = [];
 var data_EU = {};
 
-var default_color = {}
+var default_color = {};
 Object.keys(Countries).map(l => {
-    var d_f = { 'fillKey': 'defaultFill'}
-    default_color[Countries[l]] = d_f;
+  var d_f = { fillKey: 'defaultFill' };
+  default_color[Countries[l]] = d_f;
 });
 
 Object.keys(Countries).map(function(key) {
-	if (countries_EU.indexOf(key) > 0) {
-		countries_EU_shortcode.push(Countries[key]);
-	}
+  if (countries_EU.indexOf(key) > 0) {
+    countries_EU_shortcode.push(Countries[key]);
+  }
 });
 
 countries_EU_shortcode.forEach(code => {
-	data_EU[code] = { fillKey: "g3" };
+  data_EU[code] = { fillKey: 'g3' };
 });
 
-
-data_EU["CHN"] = { fillKey: "g1" };
-data_EU["TWN"] = { fillKey: "g3" };
-data_EU["USA"] = { fillKey: "g0" };
-data_EU["KOR"] = { fillKey: "g0" };
-data_EU["SWE"] = { fillKey: "g4" };
-data_EU["DNK"] = { fillKey: "g3" };
-data_EU["RUS"] = { fillKey: "g3" };
-data_EU["CAN"] = { fillKey: "g4" };
-data_EU["FRA"] = { fillKey: "g3" };
-data_EU["FIN"] = { fillKey: "g4" };
-data_EU["UKR"] = { fillKey: "g4" };
-data_EU["GBR"] = { fillKey: "g0" };
-data_EU["AUS"] = { fillKey: "g3" };
-data_EU["BRA"] = { fillKey: "g4" };
-data_EU["POL"] = { fillKey: "g4" };
-data_EU["ECU"] = { fillKey: "g5" };
-data_EU["CHL"] = { fillKey: "g5" };
-data_EU["ZAF"] = { fillKey: "g5" };
-data_EU["SAU"] = { fillKey: "g5" };
-data_EU["DEU"] = { fillKey: "g1"};
-data_EU["PHL"] = {fillKey: "g2"};
-
-
-
+data_EU['CHN'] = { fillKey: 'g1' };
+data_EU['TWN'] = { fillKey: 'g3' };
+data_EU['USA'] = { fillKey: 'g0' };
+data_EU['KOR'] = { fillKey: 'g0' };
+data_EU['SWE'] = { fillKey: 'g4' };
+data_EU['DNK'] = { fillKey: 'g3' };
+data_EU['RUS'] = { fillKey: 'g3' };
+data_EU['CAN'] = { fillKey: 'g4' };
+data_EU['FRA'] = { fillKey: 'g3' };
+data_EU['FIN'] = { fillKey: 'g4' };
+data_EU['UKR'] = { fillKey: 'g4' };
+data_EU['GBR'] = { fillKey: 'g0' };
+data_EU['AUS'] = { fillKey: 'g3' };
+data_EU['BRA'] = { fillKey: 'g4' };
+data_EU['POL'] = { fillKey: 'g4' };
+data_EU['ECU'] = { fillKey: 'g5' };
+data_EU['CHL'] = { fillKey: 'g5' };
+data_EU['ZAF'] = { fillKey: 'g5' };
+data_EU['SAU'] = { fillKey: 'g5' };
+data_EU['DEU'] = { fillKey: 'g1' };
+data_EU['PHL'] = { fillKey: 'g2' };
 
 var map = new Datamap({
-	scope: "world",
-	element: document.getElementById("container1"),
-	projection: "mercator",
-	height: 600,
-	fills: {
-		defaultFill: "#c2bdc5",
-		alpha25: "rgba(0, 0, 255, 0.25)",
-		alpha50: "rgba(0, 0, 255, 0.50)",
-        alpha65: "rgba(0, 0, 255, 0.65)",
-        highest: "rgba(127, 39, 4, 1)",
-        alpha100: "blue",
-        g0: "rgb(127,39,4)",
-        g1: "rgb(166,54,3)",
-        g2: "rgb(217,72,1)",
-        g3: "rgb(241,105,19)",
-        g4: "rgb(253,141,60)",
-        g5: "rgb(253,174,107)",
-        g6: "rgb(253,208,162)"
-	},
+  scope: 'world',
+  element: document.getElementById('container1'),
+  projection: 'mercator',
+  height: 600,
+  fills: {
+    defaultFill: '#c2bdc5',
+    alpha25: 'rgba(0, 0, 255, 0.25)',
+    alpha50: 'rgba(0, 0, 255, 0.50)',
+    alpha65: 'rgba(0, 0, 255, 0.65)',
+    highest: 'rgba(127, 39, 4, 1)',
+    alpha100: 'blue',
+    g0: 'rgb(127,39,4)',
+    g1: 'rgb(166,54,3)',
+    g2: 'rgb(217,72,1)',
+    g3: 'rgb(241,105,19)',
+    g4: 'rgb(253,141,60)',
+    g5: 'rgb(253,174,107)',
+    g6: 'rgb(253,208,162)'
+  },
 
-	data: data_EU
+  data: data_EU
 });
 
 //sample of the arc plugin
@@ -109,17 +106,17 @@ var map = new Datamap({
 //  });
 
 var data = {
-    CHN: {fillKey: 'gt50' },
-    USA: {fillKey: 'lt50' },
-    RUS: {fillKey: 'lt50' },
-    CAN: {fillKey: 'lt50' },
-    BRA: {fillKey: 'gt50' },
-    ARG: {fillKey: 'gt50' },
-    COL: {fillKey: 'gt50' },
-    AUS: {fillKey: 'gt50' },
-    ZAF: {fillKey: 'gt50' },
-    MAD: {fillKey: 'gt50' }
-}
+  CHN: { fillKey: 'gt50' },
+  USA: { fillKey: 'lt50' },
+  RUS: { fillKey: 'lt50' },
+  CAN: { fillKey: 'lt50' },
+  BRA: { fillKey: 'gt50' },
+  ARG: { fillKey: 'gt50' },
+  COL: { fillKey: 'gt50' },
+  AUS: { fillKey: 'gt50' },
+  ZAF: { fillKey: 'gt50' },
+  MAD: { fillKey: 'gt50' }
+};
 
 /**
  * {
@@ -134,126 +131,135 @@ var data = {
  */
 
 function getCountryShortKey(value) {
-    return Countries[value];
+  return Countries[value];
 }
 
- function scaler(val, min, max, yMax, yMin) {
-     var percent = (val - yMin) / (yMax - yMin);
-    return percent * (max - min) + min;
- }
+function scaler(val, min, max, yMax, yMin) {
+  var percent = (val - yMin) / (yMax - yMin);
+  return percent * (max - min) + min;
+}
 
- function scaleToMinMax(data, col, min, max) {
-    min = min || 24;
-    max = max || 55;
-    var values = data.map(obj => obj[col]);
-    // console.log(values)
-    var d_max = Math.max(...values)
-    var d_min = Math.min(...values)
+function scaleToMinMax(data, col, min, max) {
+  min = min || 24;
+  max = max || 55;
+  var values = data.map(obj => obj[col]);
+  // console.log(values)
+  var d_max = Math.max(...values);
+  var d_min = Math.min(...values);
 
-    // console.log(d_max)
+  // console.log(d_max)
 
-    return data.map(obj => {
-        return {
-            centered: getCountryShortKey(obj.country),
-            country: obj.country,
-            radius: scaler(obj[col], min, max, d_max, d_min),
-            prize: obj[col],
-            fillKey: 'alpha65'
-            // latitude: getCityLat(obj.country),
-            // longitude: getCityLon(obj.country)
-        }
-    });    
- }
-
+  return data.map(obj => {
+    return {
+      centered: getCountryShortKey(obj.country),
+      country: obj.country,
+      radius: scaler(obj[col], min, max, d_max, d_min),
+      prize: obj[col],
+      fillKey: 'alpha65'
+      // latitude: getCityLat(obj.country),
+      // longitude: getCityLon(obj.country)
+    };
+  });
+}
 
 d3.select('#update').on('click', function(e) {
-    var prom = getTournamentAggregate();
+  var prom = getTournamentAggregate();
 
-    prom.then(results => {
-        var bubble_data = scaleToMinMax(results, "sum(`prizePool`)")
-        // console.log(bubble_data)
-        
-        map.bubbles(bubble_data, {
-            popupTemplate: function(geo, data) {
-                return "<div class='hoverinfo'>Total tournament prize for this location " + data.prize + "</div>";
-            }
-        });
-        
-        map.updateChoropleth([]);
+  prom.then(results => {
+    var bubble_data = scaleToMinMax(results, 'sum(`prizePool`)');
+    // console.log(bubble_data)
+
+    map.bubbles(bubble_data, {
+      popupTemplate: function(geo, data) {
+        return (
+          "<div class='hoverinfo'>Total tournament prize for this location " +
+          data.prize +
+          '</div>'
+        );
+      }
     });
+
+    map.updateChoropleth([]);
+  });
 });
 
 var BEGIN_DATE = 2014; // 2015 Jan
 // get the number of months since BEGIN_DATE
 function getMonthOffset(row) {
-    if (row.year < BEGIN_DATE) {
-        var offset = 0;
-    } else {
-        var offset = (row.year - BEGIN_DATE) * 12 + row.month;
-    }
+  if (row.year < BEGIN_DATE) {
+    var offset = 0;
+  } else {
+    var offset = (row.year - BEGIN_DATE) * 12 + row.month;
+  }
 
-    return {
-        country: row.country,
-        prize: row.prize,
-        offset
-    }
+  return {
+    country: row.country,
+    prize: row.prize,
+    offset
+  };
 }
 
-var d_temporal = []
+var d_temporal = [];
 getTournamentByAggregateTime().then(results => {
-    // country, year, month, prize
-    d_temporal = results[0].map(getMonthOffset);
+  // country, year, month, prize
+  d_temporal = results[0].map(getMonthOffset);
 });
 
 function offsetToDate(offset) {
-    var year = Math.floor(offset / 12) + BEGIN_DATE;
-    var month = offset % 12;
+  var year = Math.floor(offset / 12) + BEGIN_DATE;
+  var month = offset % 12;
 
-    return year + '-' + month;
+  return year + '-' + month;
 }
 
 d3.select('#slider').on('change', function(e) {
-    var t_offset = parseInt(document.getElementById("slider").value);
-    document.getElementById("currDate").innerText = offsetToDate(t_offset);
+  var t_offset = parseInt(document.getElementById('slider').value);
+  document.getElementById('currDate').innerText = offsetToDate(t_offset);
 
-    var thistime = d_temporal.filter(row => {
-        return row.offset == t_offset;
-    });
+  var thistime = d_temporal.filter(row => {
+    return row.offset == t_offset;
+  });
 
-    var bubbles = scaleToMinMax(thistime, "prize");
+  var bubbles = scaleToMinMax(thistime, 'prize');
 
-    // console.log(bubbles.length)
-    var values = bubbles.map(x => x.prize);
-    // console.log(values)
-    var d_max = Math.max(...values);
-    var colors = {}
-    bubbles.forEach(bubble => {
-        var country = bubble.centered;
-        var ratio = bubble.prize/d_max;
-        var fillKey = '';
+  // console.log(bubbles.length)
+  var values = bubbles.map(x => x.prize);
+  // console.log(values)
+  var d_max = Math.max(...values);
+  var colors = {};
+  bubbles.forEach(bubble => {
+    var country = bubble.centered;
+    var ratio = bubble.prize / d_max;
+    var fillKey = '';
 
-        if(ratio >= 0.95) {
-            fillKey = "g0";
-        } else if(ratio >= 0.85) {
-            fillKey = "g1";
-        } else if(ratio >= 0.7) {
-            fillKey = "g2";
-        } else if(ratio >= 0.55) {
-            fillKey = "g3";
-        } else if(ratio >= 0.4) {
-            fillKey = "g4";
-        } else {
-            fillKey = "g5"
-        }
-        colors[country] = { 'fillKey': fillKey }
-    });
+    if (ratio >= 0.95) {
+      fillKey = 'g0';
+    } else if (ratio >= 0.85) {
+      fillKey = 'g1';
+    } else if (ratio >= 0.7) {
+      fillKey = 'g2';
+    } else if (ratio >= 0.55) {
+      fillKey = 'g3';
+    } else if (ratio >= 0.4) {
+      fillKey = 'g4';
+    } else {
+      fillKey = 'g5';
+    }
+    colors[country] = { fillKey: fillKey };
+  });
 
-    map.bubbles(bubbles, {
-        popupTemplate: function(geo, data) {
-            return "<div class='hoverinfo'>Total tournament prize for " + data.country + ": " + data.prize + "</div>";
-        }
-    });
+  map.bubbles(bubbles, {
+    popupTemplate: function(geo, data) {
+      return (
+        "<div class='hoverinfo'>Total tournament prize for " +
+        data.country +
+        ': ' +
+        formatter.format(data.prize) +
+        '</div>'
+      );
+    }
+  });
 
-    map.updateChoropleth(default_color);
-    map.updateChoropleth(colors);
+  map.updateChoropleth(default_color);
+  map.updateChoropleth(colors);
 });
---
+++ client/src/scripts/utils.js
@@ -0,0 +1,10 @@
+// Create our number formatter.
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+  // the default value for minimumFractionDigits depends on the currency
+  // and is usually already 2
+});
+
+export { formatter };
